0f3e034036ef6f5d09548bb9236b1c8696a41e85
hsrazna <anzar.terek@gmail.com>
2017-02-20 15:55:45 +0300

    services sidebar

--- wp-content/themes/houzez/single-services.php
+++ wp-content/themes/houzez/single-services.php
@@ -21,7 +21,7 @@ $sticky_sidebar = houzez_option('sticky_sidebar');
                     </div>
                 </div>
             </div>
-            <!-- <div class="col-lg-8 col-md-8 col-sm-12 col-xs-12 container-contentbar"> -->
+            <div class="col-lg-8 col-md-8 col-sm-12 col-xs-12 container-contentbar">
             <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
                 <div class="article-main">
                     
@@ -78,8 +78,9 @@ $sticky_sidebar = houzez_option('sticky_sidebar');
                     </div>
                 </div>
             </div>
-            <!-- <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12 col-md-offset-0 col-sm-offset-3 container-sidebar  <?php //if( isset( $sticky_sidebar['default_sidebar'] ) && $sticky_sidebar['default_sidebar'] != 0 ){ echo 'houzez_sticky'; }?>">
-                <?php //get_sidebar(); ?> -->
+            </div>
+            <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12 col-md-offset-0 col-sm-offset-3 container-sidebar  <?php if( isset( $sticky_sidebar['default_sidebar'] ) && $sticky_sidebar['default_sidebar'] != 0 ){ echo 'houzez_sticky'; }?>">
+                <?php get_sidebar(); ?>
             </div>
         </div>
 
